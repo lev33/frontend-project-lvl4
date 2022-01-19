0811508338057c2927d9312449cda028bd540a28
lev33 <48180783+lev33@users.noreply.github.com>
2022-01-19 22:18:45 +0300

    add AuthorizationContext

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter, Routes, Route, Link,
 } from 'react-router-dom';
 
 import LoginPage from './LoginPage.jsx';
+import AuthorizationContext from '../context/AuthorizationContext.jsx';
 
 export default function App() {
+  const userData = JSON.parse(localStorage.getItem('loggedUser'));
+  const [user, setUser] = useState(userData ? { username: userData.username } : null);
+
+  const logIn = (data) => {
+    localStorage.setItem('loggedUser', JSON.stringify(data));
+    setUser({ username: data.username });
+  };
+
+  const logOut = () => {
+    localStorage.removeItem('loggedUser');
+    setUser(null);
+  };
+
+  // logOut();
+
   return (
-    <BrowserRouter>
-      <div>
-        <h1>Welcome to React Router!</h1>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthorizationContext.Provider value={{
+      user, logIn, logOut,
+    }}
+    >
+      <BrowserRouter>
+        <div>
+          <h1>Welcome to React Router!</h1>
+          <Routes>
+            <Route path="/" element={user ? <Home /> : <LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthorizationContext.Provider>
   );
 }
 
@@ -34,7 +55,7 @@ function Home() {
   );
 }
 
-function NotFound() {
+function NoMatch() {
   return (
     <>
       <main>
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,97 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
-  Link,
+  Link, useNavigate,
 } from 'react-router-dom';
 import {
   Formik, Field, Form, ErrorMessage,
 } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
 
-export default () => (
-  <>
-    <main>
-      <h2>Login page</h2>
-      <Formik
-        initialValues={{ name: '', password: '' }}
-        validationSchema={Yup.object({
-          name: Yup.string()
-            .max(15, 'Must be 15 characters or less')
-            .required('Required'),
-          password: Yup.string()
-            .max(20, 'Must be 20 characters or less')
-            .required('Required'),
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        <Form>
-          <label htmlFor="name">Name</label>
-          <Field name="name" type="text" />
-          <ErrorMessage name="name" />
+import AuthorizationContext from '../context/AuthorizationContext.jsx';
 
-          <label htmlFor="password">Password</label>
-          <Field name="password" type="text" />
-          <ErrorMessage name="password" />
-
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </main>
-    <nav>
-      <Link to="/">Home</Link>
-    </nav>
-  </>
-);
-
-/*
-import React from 'react';
-import {
-  Link,
-} from 'react-router-dom';
-import {
-  Formik, Form, Field, ErrorMessage,
-} from 'formik';
-
-export default function Login() {
+const LoginPage = () => {
+  const { logIn } = useContext(AuthorizationContext);
+  const navigate = useNavigate();
   return (
     <>
       <main>
         <h2>Login page</h2>
         <Formik
-          initialValues={{ email: '', password: '' }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
+          initialValues={{ name: '', password: '' }}
+          validationSchema={Yup.object({
+            username: Yup.string()
+              .max(15, 'Must be 15 characters or less')
+              .required('Required'),
+            password: Yup.string()
+              .max(20, 'Must be 20 characters or less')
+              .required('Required'),
+          })}
+          onSubmit={async (value) => {
+            try {
+              const { data } = await axios.post('/api/v1/login', value);
+              logIn(data);
+              navigate('/');
+            } catch (err) {
+              console.log(err.response);
             }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
           }}
         >
-          {({ isSubmitting }) => (
-            <Form>
-              <Field type="email" name="email" />
-              <ErrorMessage name="email" component="div" />
-              <Field type="password" name="password" />
-              <ErrorMessage name="password" component="div" />
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </Form>
-          )}
+          <Form>
+            <label htmlFor="username">User Name</label>
+            <Field name="username" type="text" />
+            <ErrorMessage name="username" />
+
+            <label htmlFor="password">Password</label>
+            <Field name="password" type="text" />
+            <ErrorMessage name="password" />
+
+            <button type="submit">Submit</button>
+          </Form>
         </Formik>
       </main>
       <nav>
@@ -99,5 +55,6 @@ export default function Login() {
       </nav>
     </>
   );
-}
-*/
+};
+
+export default LoginPage;
