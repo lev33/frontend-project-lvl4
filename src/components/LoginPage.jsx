ff182632ff3d9c87e8013f0a4fe9dcb773fbb402
lev33 <48180783+lev33@users.noreply.github.com>
2022-01-19 14:25:05 +0300

    add yup

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -3,6 +3,56 @@ import {
   Link,
 } from 'react-router-dom';
 import {
+  Formik, Field, Form, ErrorMessage,
+} from 'formik';
+import * as Yup from 'yup';
+
+export default () => (
+  <>
+    <main>
+      <h2>Login page</h2>
+      <Formik
+        initialValues={{ name: '', password: '' }}
+        validationSchema={Yup.object({
+          name: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Required'),
+          password: Yup.string()
+            .max(20, 'Must be 20 characters or less')
+            .required('Required'),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <label htmlFor="name">Name</label>
+          <Field name="name" type="text" />
+          <ErrorMessage name="name" />
+
+          <label htmlFor="password">Password</label>
+          <Field name="password" type="text" />
+          <ErrorMessage name="password" />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </main>
+    <nav>
+      <Link to="/">Home</Link>
+    </nav>
+  </>
+);
+
+/*
+import React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+import {
   Formik, Form, Field, ErrorMessage,
 } from 'formik';
 
@@ -50,3 +100,4 @@ export default function Login() {
     </>
   );
 }
+*/
